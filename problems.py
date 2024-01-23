3a88d814a5271f81c54b90c09d0fdf516f68877f
minimario <gua@mit.edu>
2024-01-22 20:13:21 -0500

    update with new problems

--- problems.py
+++ problems.py
@@ -15,31 +15,30 @@ Now, to find the total number of clips sold in April and May, we add the two amo
 Natalia sold a total of 72 clips in April and May combined.
 Final Answer: The answer is $\\boxed{72}$."""
 
-# answer: 60
-prompt_without_retrieval_1 = inst + """\n\nProblem: What is the greatest common divisor of all integers that can be represented as $k^6-k^2$, where k is a positive integer?"""
-
-prompt_with_retrieval_1 = inst + """\n\nProblem: What is the greatest common divisor of all integers that can be represented as $k^3-k$, where k is a positive integer?
+# Problem 1
+problem_without_retrieval_1 = inst + """\n\nProblem: What is the greatest common divisor of all integers that can be represented as $k^6-k^2$, where k is a positive integer?"""
+problem_with_retrieval_1 = inst + """\n\nProblem: What is the greatest common divisor of all integers that can be represented as $k^3-k$, where k is a positive integer?
 
 Solution: When $k=2$, the expression is equal to $2^3-2=6$. Next, note that $k^3-k=k(k-1)(k+1)$. This is divisible by 2, because one of $k$ and $k+1$ has to be divisible by $2$. This is also divisible by $3$ because one of $k-1, k, k+1$ is divisible by $3$. Therefore, it must be divisible by $6$. 
 Final Answer: The final answer is $\\boxed{6}$.
  
 Problem: What is the greatest common divisor of all integers that can be represented as $k^6-k^2$, where k is a positive integer?"""
+answer_1 = 60
 
-# answer: 506
-prompt_without_retrieval_2 = inst + """\n\nProblem: How many divisors of $6^2023$ have a units digit of $1$?"""
-
-prompt_with_retrieval_2 = inst + """\n\nProblem: How many divisors of $10^{100}$ have a units digit of $5$?
+# Problem 2
+problem_without_retrieval_2 = inst + """\n\nProblem: How many divisors of $6^2023$ have a units digit of $1$?"""
+problem_with_retrieval_2 = inst + """\n\nProblem: How many divisors of $10^{100}$ have a units digit of $5$?
 Solution: We have that $10^{100} = 2^{100} \cdot 5^{100}$. If a divisor of $10^{100}$ is even, then it cannot have a units digit of $5$.
 
 Therefore, all divisors with a units digit of $5$ must be of the form $5^{k}$, for $k=1, 2, \cdots, 100$. Therefore, thre are 100 divisors of $10^{100}$ with a units digit of $5$.
 Final Answer: The final answer is $\\boxed{100}$.
 
 Problem: How many divisors of $6^{2023}$ have a units digit of $1$?"""
+answer_2 = 506
 
-# answer: 5
-prompt_without_retrieval_3 = inst + """\n\nProblem: How many ordered pairs $(m, n)$ of positive integers satisfy $m^n = 6^{16}$?"""
-
-prompt_with_retrieval_3 = inst + """\n\nProblem: How many ordered pairs $(x, y)$ of positive integers satisfy $x^y = 4^{10}$?
+# Problem 3
+problem_without_retrieval_3 = inst + """\n\nProblem: How many ordered pairs $(m, n)$ of positive integers satisfy $m^n = 6^{16}$?"""
+problem_with_retrieval_3 = inst + """\n\nProblem: How many ordered pairs $(x, y)$ of positive integers satisfy $x^y = 4^{10}$?
 
 Solution: We have that $x^y = 4^{10} = 2^{20}$. Note that $x$ must be a power of $2$, because $2^{20}$ has no other prime factors. Therefore, we can have $x=2^a$, so that $x^y = 2^{ay} = 2^{20}$. 
 
@@ -47,10 +46,11 @@ Here, $a$ and $y$ can be anything, as long as $ay = 20$. Therefore, we just need
 Final Answer: The final answer is $\\boxed{6}$.
 
 Problem: How many ordered pairs $(m, n)$ of positive integers satisfy $m^n = 6^{16}$?"""
+answer_3 = 5
 
-prompt_without_retrieval_4 = inst + """\n\nProblem: Determine the least positive integer $n$ for which the following statement is true: the product of any $n$ odd consecutive positive integers is divisible by $45$."""
-
-prompt_with_retrieval_4 = inst + """Problem: What is the least $n$ such that the product of any $n$ even consecutive positive integers is divisible by $56$?
+# Problem 4
+problem_without_retrieval_4 = inst + """\n\nProblem: Determine the least positive integer $n$ for which the following statement is true: the product of any $n$ odd consecutive positive integers is divisible by $45$."""
+problem_with_retrieval_4 = inst + """\n\nProblem: What is the least $n$ such that the product of any $n$ even consecutive positive integers is divisible by $56$?
 
 Solution: If something is divisible by $56$, then it must be divisible by $8$ and $7$. 
 
@@ -63,6 +63,82 @@ Therefore, when $n=7$, the statement is true. For $n=6$, observe that $2 \cdot 4
 Final Answer: The final answer is $\\boxed{7}$.
 
 Problem: Determine the least positive integer $n$ for which the following statement is true: the product of any $n$ odd consecutive positive integers is divisible by $45$."""
-# prompt_without_retrieval_4 = inst + """\n\nProblem: What is the least possible positive sum of three integers multiplying to 2025? The integers are not necessarily positive."""
+answer_4 = 6
 
-# prompt_without_retrieval_3 = inst + """\n\nProblem: How many ordered pairs $(m, n)$ of positive integers satisfy $m^n = 6^{16}$?"""
+# Problem 5
+problem_without_retrieval_5 = inst + """\n\nProblem: Two angles of an isosceles triangle measure $80^{\circ}$ and $x^{\circ}$. What is the sum of all the possible values of $x$?"""
+problem_with_retrieval_5 = inst + """\n\nProblem: Two angles of an isosceles triangle measure $30^{\circ}$ and $x^{\circ}$. What is the sum of all the possible values of $x$?
+
+Solution: Two of the angles must be equal. If the $30^{\circ}$ angle is one of them, then the angles must be $30^{\circ}, 30^{\circ}$, and $180^{\circ} - 30^{\circ} - 30^{\circ} = 120^{\circ}$. If the $30^{\circ}$ angle is not one of them, then the other two angles must sum up to $180^{\circ} - 30^{\circ} = 150^{\circ}$, so each angle must be $75^{\circ}$. Therefore, the possibilities are $30^{\circ}, 120^{\circ}$, and $75^{\circ}$, so the answer is $30 + 120 + 75 = 225$.
+
+Final Answer: The final answer is $\\boxed{225}$.
+
+Problem: Two angles of an isosceles triangle measure $80^{\circ}$ and $x^{\circ}$. What is the sum of all the possible values of $x$?"""
+answer_5 = 150
+
+# Problem 6
+problem_without_retrieval_6 = inst + """\n\nProblem: There are two points, $A = (b, 22)$ and $B = (2, 450)$ for some integer $b$. For how many values of $b$ is the slope of AB an integer?"""
+problem_with_retrieval_6 = inst + """\n\nProblem: There are two points, $A = (1, 211)$ and $B = (b, 2011)$ for some integer $b$. For how many values of $b$ is the slope of AB an integer?
+
+Solution: The slope $m = \frac{2011 - 211}{b - 1} = \frac{1800}{b - 1}$ is an integer, so $(b - 1)$ divides $1800$. If $x$ is a positive factor of $1800$, then $1 + x$ and $1 - x$ are solutions for $b$, so each positive factor of $1800$ should be counted twice. Because $23 \cdot 3^2 \cdot 5^2 = 1800$, we have $(4 \cdot 3 \cdot 3) = 36$ positive factors of $1800$, and therefore $2 \cdot 36 = 72$ possible values of $b$.
+
+Final Answer: The final answer is $\\boxed{72}$
+
+Problem: There are two points, $A = (b, 22)$ and $B = (2, 450)$ for some integer $b$. For how many values of $b$ is the slope of AB an integer?"""
+answer_6 = 12
+
+# Problem 7
+problem_without_retrieval_7 = inst + """\n\nProblem: For how many integers $d$ in the set ${142, 143, . . . , 198}$ is the area of a rectangle with perimeter 400 and diagonal length d an integer?"""
+problem_with_retrieval_7 = inst + """\n\nProblem: For how many integers $d$ in the set ${60, 61, . . . , 70}$ is the area of a rectangle with perimeter 160 and diagonal length $d$ an integer?
+
+Solution: Let $x$ and $y$ be the side lengths of the rectangle. Then, the area is $xy$, the perimeter is $2x+2y=160$, and $x^2+y^2=d^2$ by the Pythagorean theorem. Since $2x+2y=160$, we have $x+y=80$. Note that $(x+y)^2 = x^2 + 2xy + y^2 = d^2 + 2xy$, so $xy = \frac{(x+y)^2 - d^2}{2} = \frac{80^2 - d^2}{2}$. Therefore, the area $xy$ is an integer if and only if $d$ is even. There are $\frac{70-60}{2} + 1 = 5 + 1 = 6$ even integers between $60$ and $70$, so the answer is $6$.
+
+Final Answer: The final answer is $\\boxed{6}$.
+
+Problem: For how many integers $d$ in the set ${142, 143, . . . , 198}$ is the area of a rectangle with perimeter 400 and diagonal length d an integer?"""
+answer_7 = 29
+
+# Problem 8
+problem_without_retrieval_8 = inst + """\n\nProblem: Let $(x, y)$ be an ordered pair of real numbers satisfying $-x^2+3y^2-5x+7y+4=0$ and $2x^2-2y^2-x+y+21=0$. What is the sum of all possible values of $x+y$?"""
+problem_with_retrieval_8 = inst + """\n\nProblem: Let $(x, y)$ be an ordered pair of real numbers satisfying $2x^2-4y^2-2x+4y+2=0$ and $-x^2+5y^2+2y+8=0$. What is the sum of all possible values of $x+y$?
+
+Solution: If we add the two equations, we get $x^2+y^2-2x+6y+10=0$. Completing the square, this can be rewritten as $(x-1)^2+(y+3)^2=0$, Since squares must be nonnegative, we must have $(x-1)^2=0$ and $(y+3)^2=0$, which means that $(x, y) = (1, -3)$ is the only solution. Therefore, $x+y=-2$ is the only possible value.
+
+Final Answer: The final answer is $\\boxed{-2}$
+
+Problem: Let $(x, y)$ be an ordered pair of real numbers satisfying $-x^2+3y^2-5x+7y+4=0$ and $2x^2-2y^2-x+y+21=0$. What is the sum of all possible values of $x+y$?"""
+answer_8 = -1
+
+# Problem 9
+problem_without_retrieval_9 = inst + """\n\nProblem: There is a rectangular box of surface area 44 whose space diagonals have length 10. Find the sum of the lengths of all the edges of the box."""
+problem_with_retrieval_9 = inst + """\n\nProblem: There is a rectangular box of surface area 27 whose space diagonals have length 13. Find the sum of the lengths of all the edges of the box.
+
+Solution: Let the rectangular box have edge lengths $a$, $b$, and $c$. Then, the surface area is $2ab + 2bc + 2ca = 27$ and the space diagonal is $\sqrt{a^2 + b^2 + c^2} = 13$, so $a^2 + b^2 + c^2 = 169$. Adding the two equations yields $a^2 + b^2 + c^2 + 2ab + 2bc + 2ca = (a + b + c)^2 = 169 + 27 = 196$, which implies that $a + b + c = \sqrt{196} = 14$. The sum of the lengths of the edges is $4(a + b + c) = 56$.
+
+Final Answer: The final answer is $\\boxed{56}$.
+
+Problem: There is a rectangular box of surface area 44 whose space diagonals have length 10. Find the sum of the lengths of all the edges of the box."""
+answer_9 = 48
+
+# Problem 10
+problem_without_retrieval_10 = inst + """\n\nProblem: In how many ways can Weijia write the integers 1, 2, 3, 4, 5, 6 on the floor such that no single swap of two numbers would lead to the sequence 1, 2, 3, 4, 5, 6? For example, 6, 5, 4, 3, 2, 1 is one such sequence, but 3, 2, 1, 4, 5, 6 is not, because swapping the 3 and the 1 would lead to 1, 2, 3, 4, 5, 6."""
+problem_with_retrieval_10 = inst + """\n\nProblem: In how many ways can Weijia write the integers 1, 2, 3, 4 on the floor such that no single swap of two numbers would lead to the sequence 1, 2, 3, 4? For example, 4, 3, 2, 1 is one such sequence, but 3, 2, 1, 4 is not, because swapping the 3 and the 1 would lead to 1, 2, 3, 4.
+
+Solution: We can use complementary counting to solve this problem. There are 4! = 24 ways to write the 4 integers 1, 2, 3, 4 on the floor. Next, we need to count the number of permutations of 1, 2, 3, 4 that are one swap away from 1, 2, 3, and 4. This is the same as the number of ways to swap two numbers in 1, 2, 3, 4, which is $\binom{4}{2} = \frac{4!}{2! \cdot (4-2)!} = \frac{24}{4} = 6$. Therefore, the answer is $24 - 6 = 18$.
+
+Final Answer: The final answer is $\\boxed{18}$. 
+
+Problem: In how many ways can Weijia write the integers 1, 2, 3, 4, 5, 6 on the floor such that no single swap of two numbers would lead to the sequence 1, 2, 3, 4, 5, 6? For example, 6, 5, 4, 3, 2, 1 is one such sequence, but 3, 2, 1, 4, 5, 6 is not, because swapping the 3 and the 1 would lead to 1, 2, 3, 4, 5, 6."""
+answer_10 = 705
+
+problem_without_retrieval_11 = inst + """\n\nProblem: Find the smallest positive integer N such that 2N is a perfect square and 3N is a perfect cube."""
+problem_with_retrieval_11 = inst + """\n\nProblem: Find the smallest positive integer N such that $5N$ is a perfect square and $2N$ is a perfect cube.
+
+Solution: Let's consider the prime factorization of $N$. First, notice that $N$ should only have powers of $2$ and $5$. Therefore, let $N = 2^x \cdot 5^y$. If $5N = 2^x \cdot 5^{y+1}$ is a perfect square, then $x$ and $y+1$ must be even. If $2N = 2^{x+1} \cdot 5^y$ is a perfect cube, then $x+1$ and $y$ must be multiples of $3$. 
+
+Now, let's find the smallest $x$ and $y$. Since $x$ is even and $x+1$ is a multiple of $3$, $x=2$ is the smallest possibility. Since $y+1$ is even and $y$ is a multiple of $3$, $y=3$ is the smallest possibility. Therefore, $N = 2^2 \cdot 5^3 = 4 \cdot 125 = 500$ is the smallest.
+
+Final Answer: The final answer is $\\boxed{500}$.
+
+Problem: Find the smallest positive integer N such that 2N is a perfect square and 3N is a perfect cube."""
+answer_11 = 72
